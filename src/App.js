b9703d6cf19bd424bb3f062febcf364aa09e9263
capricornus44 <antonzavalniuk@gmail.com>
2021-04-02 14:40:14 +0200

    install uuid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-const App = () => {
-  return <div>App</div>
+import React, { Component } from "react"
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  }
 }
 
 export default App
